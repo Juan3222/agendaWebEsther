c3f3cf7b6dd628270c050e7a67e1147d744dbcfb
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2024-02-07 11:31:57 -0300

    Versión 0.2.3: Se cambió el idioma al calendario

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import { es } from "dayjs/locale/es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
+dayjs.locale("es");
 
 export default function () {
   const localizer = dayjsLocalizer(dayjs);
+  const messages = {
+    allDay: "Todo el día",
+    previous: "Anterior",
+    next: "Siguiente",
+    today: "Hoy",
+    month: "Mes",
+    week: "Semana",
+    day: "Día",
+    agenda: "Agenda",
+    date: "Fecha",
+    time: "Hora",
+    event: "Evento",
+    noEventsInRange: "Sin eventos",
+  };
   return (
     <div className="calendar">
-      <Calendar localizer={localizer} />
+      <Calendar localizer={localizer} messages={messages} />
     </div>
   );
 }
