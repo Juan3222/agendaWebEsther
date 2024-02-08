2193daf80c982c575b210e23d25aef716ceec8a2
Renzo Pereira <renzogpereira@gmail.com>
2024-02-08 17:57:26 -0300

    Versión 0.3: Cree el proyecto en Firebase, lo vincule en el archivo config dentro de la carpeta Firebase y cree un useEfect en el que llamo a la colección y seteo el primer evento

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -26,27 +26,6 @@ export default function YourComponent() {
   };
 
   const localizer = dayjsLocalizer(dayjs);
-
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const docRef = doc(db, "eventos", "sCE388WNBBBqf4DUIOus"); // Creo constante que iguala la db (database), coleccion "eventos" y la id de uno de los documentos (desp hay que cambiarlo por useParams)
-    getDoc(docRef)
-      .then((resp) => {
-        const data = resp.data(); // Despues de resolver la promesa creo data = a la data de firebase
-        const formattedData = {
-          // Le pedi a chat gpt que me cambie el formato por que traia la fecha en segundos
-          start: dayjs.unix(data.start.seconds).toDate(),
-          end: dayjs.unix(data.end.seconds).toDate(),
-          title: data.title,
-        };
-        setEvents([formattedData]); // Guardardo los eventos en el estado
-      })
-      .catch((error) => {
-        console.error("Error getting document:", error);
-      });
-  }, []);
-
   return (
     <div className="calendar">
       <Calendar localizer={localizer} messages={messages} events={events} />
