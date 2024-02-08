4c6778371e829f042c7907a9d7268fdc81c43459
Renzo Pereira <renzogpereira@gmail.com>
2024-02-08 17:57:26 -0300

    Versión 0.3: Cree el proyecto en Firebase, lo vincule en el archivo config dentro de la carpeta Firebase y cree un useEfect en el que llamo a la colección y seteo el primer evento

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
 import "dayjs/locale/es";
+import { db } from "../../firebase/config";
+import { getDoc, doc } from "firebase/firestore";
 
-dayjs.locale("es")
+dayjs.locale("es");
 
-export default function () {
-
+export default function YourComponent() {
   const messages = {
     allDay: "Todo el día",
     previous: "Anterior",
@@ -24,9 +25,29 @@ export default function () {
   };
 
   const localizer = dayjsLocalizer(dayjs);
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const docRef = doc(db, "eventos", "sCE388WNBBBqf4DUIOus"); // Creo constante que iguala la db (database), coleccion "eventos" y la id de uno de los documentos (desp hay que cambiarlo por useParams)
+    getDoc(docRef)
+      .then((resp) => {
+        const data = resp.data(); // Despues de resolver la promesa creo data = a la data de firebase
+        const formattedData = { // Le pedi a chat gpt que me cambie el formato por que traia la fecha en segundos
+          start: dayjs.unix(data.start.seconds).toDate(),
+          end: dayjs.unix(data.end.seconds).toDate(),
+          title: data.title,
+        };
+        setEvents([formattedData]); // Guardardo los eventos en el estado
+      })
+      .catch((error) => {
+        console.error("Error getting document:", error);
+      });
+  }, []);
+
   return (
     <div className="calendar">
-      <Calendar localizer={localizer} messages={messages} />
+      <Calendar localizer={localizer} messages={messages} events={events} />
     </div>
   );
 }
