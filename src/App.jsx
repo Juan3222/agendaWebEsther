3eb54f2c0569d647f3e82241f1462ed9148b6cbf
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2024-03-04 20:09:50 -0300

    Versión 0.8: Se dividió todo el código del formulario a otro componente llamado EventForm, se agregaron nuevas funcionalidades, como la sincronizacón en tiempo real con fireba se, asi no se necesita actualizar la página cada vez que se agregue o se borre un evento

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Calendar from "./components/Calendar/Calendar.jsx";
 import Header from "./components/Header/Header.jsx";
 import React from "react";
 import Footer from "./components/footer/Footer.jsx";
+import EventForm from "./components/EventForm/EventForm.jsx";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Calendar />
+      <EventForm></EventForm>
       <Footer />
     </div>
   );
